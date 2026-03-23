f96b9f480533a2761c011e61749b8f0a8fc88875
sky17892 <sky17893@gmail.com>
2026-03-23 17:40:53 +0900

    ai oracle flask minus commit

--- aiml/config.py
+++ aiml/config.py
@@ -1,12 +1,22 @@
 import os
+import oracledb
+from flask_sqlalchemy import SQLAlchemy
 
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-print("BASE_DIR:", BASE_DIR)
+oracledb.init_oracle_client(lib_dir=r"C:\oraclexe\instantclient_19_25")
 
-SQLALCHEMY_DATABASE_URI = 'sqlite:///{}'.format(
-    os.path.join(BASE_DIR, 'example.db')
-)
+#BASE_DIR = os.path.dirname(__file__)
+#print("BASE_DIR",BASE_DIR)
 
-print("SQLALCHEMY_DATABASE_URI:", SQLALCHEMY_DATABASE_URI)
+#SQLALCHEMY_DATABASE_URI = 'oracle+oracledb://scott:tiger@localhost:1521/xe' + os.path.join(BASE_DIR, 'pybo.db')
+#print("SQLALCHEMY_DATABASE_URI:",SQLALCHEMY_DATABASE_URI)
 
-SQLALCHEMY_TRACK_MODIFICATIONS = False
+#SQLALCHEMY_TRACK_MODIFICATIONS = False
+#SECRET_KEY = "dev"
+BASE_DIR = os.path.dirname(__file__)
+print("BASE_DIR",BASE_DIR)
+
+SQLALCHEMY_DATABASE_URI = 'oracle+oracledb://scott:tiger@localhost:1521/xe'
+print("SQLALCHEMY_DATABASE_URI:",SQLALCHEMY_DATABASE_URI)
+
+SQLALCHEMY_TRACK_MODIFICATIONS = True
+SECRET_KEY = "dev"
