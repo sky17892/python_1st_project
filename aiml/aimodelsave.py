d618e15fbdf0dd454569e18e260646f38ee66b4a
sky17892 <sky17893@gmail.com>
2026-03-27 15:49:29 +0900

    ai pipeline commit

--- aiml/aimodelsave.py
+++ aiml/aimodelsave.py
@@ -3,5 +3,10 @@ import joblib
 from growthdata import xgb_clf
 from envdata import xgb_reg
 
-joblib.dump(xgb_clf,"growth_model.pkl")
-joblib.dump(xgb_reg,"production_model.pkl")
+growth_model =  joblib.dump(xgb_clf,"growth_model.pkl")
+production_model = joblib.dump(xgb_reg,"production_model.pkl")
+
+def run_model(df):
+    g = growth_model.predict(df)
+    p = production_model.predict(df)
+    return g, p
