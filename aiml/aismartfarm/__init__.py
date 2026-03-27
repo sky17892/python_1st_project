d5f70267791fdf000187047a232cc08a9a5c8977
sky17892 <sky17893@gmail.com>
2026-03-27 16:34:14 +0900

    ai frontbackend commit

--- aiml/aismartfarm/__init__.py
+++ aiml/aismartfarm/__init__.py
@@ -6,14 +6,28 @@ import config
 db = SQLAlchemy()
 migrate = Migrate()
 
-def index():
+#def index():
+#    app = Flask(__name__)
+#    app.config.from_object(config)
+
+#    db.init_app(app)
+#    migrate.init_app(app, db)
+
+#    from .views import main_view
+#    from . import models
+#    app.register_blueprint(main_view.bp)
+#    return app
+
+def create_app():
     app = Flask(__name__)
     app.config.from_object(config)
+    from .import models
 
     db.init_app(app)
-    migrate.init_app(app, db)
+    migrate.init_app(app,db)
 
     from .views import main_view
-    from . import models
     app.register_blueprint(main_view.bp)
+    #app.register_blueprint(question_views.bp)
+    #app.register_blueprint(answer_views.bp)
     return app
--- aiml/aismartfarm/views/main_view.py
+++ aiml/aismartfarm/views/main_view.py
@@ -1,11 +1,16 @@
-from flask import Blueprint
+from flask import Blueprint, url_for
+from flask_sqlalchemy import SQLAlchemy
+from flask import Blueprint, render_template
+from werkzeug.utils import redirect
+#from services.pipeline import run_pipeline
 
 bp = Blueprint('main', __name__, url_prefix='/')
+
+@bp.route('/')
+def index():
+    return render_template('main/result.html')
 
 @bp.route('/main')
 def hello_flask():
     return "hello, flask"
 
-@bp.route('/')
-def flask():
-    return "flask index"
